39125fe72db62838a34856a597051f3be4048541
Akshay Nair <phenax5@gmail.com>
2020-03-14 00:35:33 +0530

    Installation for /usr/bin added

--- install.sh
+++ install.sh
@@ -6,29 +6,38 @@ clone() {
   git clone https://github.com/phenax/shell-macros.git $SHELL_MACROS_PATH;
 }
 
-if [[ -f "$SHELL_MACROS_PATH" ]]; then
+if [[ -d "$SHELL_MACROS_PATH" ]]; then
   echo "You already have shell-macros installed. Upgrading to latest release";
 
-  echo "Backing up old config"
+  echo "* Backing up old config..."
   TMP_SM_PATH="${SHELL_MACROS_PATH}_backup";
+  rm -rf $TMP_SM_PATH;
   mv $SHELL_MACROS_PATH $TMP_SM_PATH;
+  echo "";
 
-  echo "Upgrading...";
+  echo "* Upgrading...";
   clone;
+  echo "";
 
-  echo "Restoring config";
+  echo "* Restoring config...";
   cp -rf "$TMP_SM_PATH/dump" "$SHELL_MACROS_PATH/dump";
   cp -rf "$TMP_SM_PATH/macros" "$SHELL_MACROS_PATH/macros";
   cp -rf "$TMP_SM_PATH/config.sh" "$SHELL_MACROS_PATH/config.sh";
 else
-  echo "Installing...";
+  echo "* Installing...";
   clone;
+
+  mkdir -p $SHELL_MACROS_PATH/dump;
+  mkdir -p $SHELL_MACROS_PATH/macros;
 fi;
 
-cd $SHELL_MACROS_PATH;
+rm -rf $SHELL_MACROS_PATH/.git;
+echo "";
 
-rm -rf .git;
+echo "* Configuring binary...";
+sudo ln -s "$SHELL_MACROS_PATH/macros.sh" /usr/bin/macros;
+echo "";
 
-mkdir -p $SHELL_MACROS_PATH/dump;
-mkdir -p $SHELL_MACROS_PATH/macros;
-
+echo "Done!";
+echo "";
+echo "Run 'macros help' to get started";
