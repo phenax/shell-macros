6f9048e0a9c06eff125a1a049afa54261608f19b
Akshay Nair <phenax5@gmail.com>
2020-03-15 15:35:06 +0530

    - to _ for conventions

--- macros.sh
+++ macros.sh
@@ -15,20 +15,22 @@ Commands:
   help                  - This dialog
 ";
 
-get-macro-path() { echo "$MACROS_LIST_PATH/$1"; }
+get_prompt() { echo "($1) >> "; }
+
+get_macro_path() { echo "$MACROS_LIST_PATH/$1"; }
 
 guard() { if [[ -z "$2" ]]; then echo "$1"; exit 1; fi; }
 
-guard-macro-name() {
+guard_macro_name() {
   guard "Invalid macro name" "$1";
 }
 
-guard-macro-not-exists() {
-  [[ ! -f "$(get-macro-path "$1")" ]] && \
+guard_macro_not_exists() {
+  [[ ! -f "$(get_macro_path "$1")" ]] && \
     guard "Macro '$1' does not exist";
 }
-guard-macro-already-exists() {
-  [[ -f "$(get-macro-path "$1")" ]] && \
+guard_macro_already_exists() {
+  [[ -f "$(get_macro_path "$1")" ]] && \
     guard "Macro '$1' already exists. To delete it, run 'macros delete $1'";
 }
 
@@ -37,12 +39,12 @@ strip_history_timestamp() { sed 's/^\:\s\+[0-9: ]\+;//g'; }
 
 # Start recording a macro
 record() {
-  guard-macro-name "$1";
-  guard-macro-already-exists "$1";
-  local macro_path=$(get-macro-path "$1");
+  guard_macro_name "$1";
+  guard_macro_already_exists "$1";
+  local macro_path=$(get_macro_path "$1");
 
   # Start new shell
-  HISTFILE="$macro_path" CUSTOM_PROMPT="($1) >> " $SHELL;
+  HISTFILE="$macro_path" CUSTOM_PROMPT="$(get_prompt "$1")" $SHELL;
 
   # Remove history timestamps if exists
   contents=$(cat "$macro_path" | strip_history_timestamp);
@@ -51,9 +53,9 @@ record() {
 
 # Delete existing macro
 delete() {
-  guard-macro-name "$1";
-  guard-macro-not-exists "$1";
-  local macro_path=$(get-macro-path "$1");
+  guard_macro_name "$1";
+  guard_macro_not_exists "$1";
+  local macro_path=$(get_macro_path "$1");
 
   # Delete session file
   rm "$macro_path";
@@ -61,9 +63,9 @@ delete() {
 
 # Execute a macro
 run() {
-  guard-macro-name "$1";
-  guard-macro-not-exists "$1";
-  local macro_path=$(get-macro-path "$1");
+  guard_macro_name "$1";
+  guard_macro_not_exists "$1";
+  local macro_path=$(get_macro_path "$1");
 
   # Read and execute each line
   cat $macro_path | $SHELL -;
