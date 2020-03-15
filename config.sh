8889611e92ed54086c5d89ccecbca07aac9000a9
Akshay Nair <phenax5@gmail.com>
2020-03-15 16:37:56 +0530

    Removes colors from prompt

--- config.sh
+++ config.sh
@@ -1,8 +1,5 @@
-MACROS_PATH="$HOME/.config/shell-macros";
-source "$MACROS_PATH/colors.sh";
-
 export SHELL="bash";
 
 # Return prompt string
-get_prompt() { echo -e "\$(${COLOR_LIGHT_GREEN}$1${COLOR_DEFAULT}) >> "; }
+get_prompt() { echo -e "\$($1) >> "; }
 
